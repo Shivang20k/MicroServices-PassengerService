fa1298d1a30751054aa63ddca7a3e8f50a9934f8
Shivang <st.shivanggupta2@gmail.com>
2024-01-07 14:48:12 +0530

    project push - passengerService

---
+++ src/test/java/com/micro/passenger/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.passenger.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
